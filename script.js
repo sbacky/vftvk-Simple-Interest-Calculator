f32869fa1e57f83f65d3136dd45a10cf89632489
Neil <nhc.crum@gmail.com>
2021-05-12 07:49:14 +0000

    Add index.html, style.css, and stript.js
    Change html to include slider for rate and drop down for years. Add functions in javascript
    for dynamic slider and validation check for principal

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // Create variables to input elements principle, rate, and years
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // Use variables to calculate interest
+    var interest = principal * years * rate / 100;
+
+    // Convert years to an actually year in the future
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // Set text for resulting interest
+    document.getElementById("result").innerHTML="If you deposit " + principal + ",\n\
+        at an interest rate of " + rate + ".\nYou will receive an amount of " + interest + ",\n\
+        in the year " + year + "\n";
+}
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function checkdata() {
+    // Get prinicpal element from form
+    var principal = document.getElementById("principal").value;
+    
+    // Check if principal field is empty
+    if(principal<1){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    return true;
 }
         
