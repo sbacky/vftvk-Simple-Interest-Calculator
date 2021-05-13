c6aad40adb560148da1d426aa50915af6ac37be4
sbacky <74469287+sbacky@users.noreply.github.com>
2021-05-13 00:50:36 -0700

    Percentage sign now displayed when slider is changed

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute()
 }
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 function checkdata() {
     // Get prinicpal element from form
